8c7630260b3e89f95eff2c0e243531f0135a9517
Anikcse18 <istiakjavedanik@gmail.com>
2024-02-22 03:42:38 +0600

    added error hadeler

--- js/deposite.js
+++ js/deposite.js
@@ -2,11 +2,15 @@ document.getElementById("btn-deposite").addEventListener("click", function () {
   // deposite update
   const depositeUserInput = getInputFieldValuebyId("deposite_input");
   const depositeBox = getOutputValueByID("deposite_ammount");
-  const depositeTotal = depositeUserInput + depositeBox;
-  setTextElementById("deposite_ammount", depositeTotal);
 
-  //Blance Update
-  const blanceBox = getOutputValueByID("total_balance");
-  const totalBlance = blanceBox + depositeUserInput;
-  setTextElementById("total_balance", totalBlance);
+  if (isNaN(depositeUserInput) || depositeUserInput < 0) {
+    alert("Invalid Input");
+  } else {
+    const depositeTotal = depositeUserInput + depositeBox;
+    setTextElementById("deposite_ammount", depositeTotal);
+    //Blance Update
+    const blanceBox = getOutputValueByID("total_balance");
+    const totalBlance = blanceBox + depositeUserInput;
+    setTextElementById("total_balance", totalBlance);
+  }
 });
--- js/withdraw.js
+++ js/withdraw.js
@@ -1,9 +1,14 @@
 document.getElementById("btn-withdraw").addEventListener("click", function () {
   const withdrawUserInput = getInputFieldValuebyId("withdraw_input");
   const withdrawBox = getOutputValueByID("withdraw_ammount");
-  const withdrawTotal = withdrawUserInput + withdrawBox;
-  setTextElementById("withdraw_ammount", withdrawTotal);
-  const totalBlance = getOutputValueByID("total_balance");
-  const totalBlanceUpdate = totalBlance - withdrawUserInput;
-  setTextElementById("total_balance", totalBlanceUpdate);
+
+  if (!isNaN(withdrawUserInput) && withdrawUserInput >= withdrawBox) {
+    const withdrawTotal = withdrawUserInput + withdrawBox;
+    setTextElementById("withdraw_ammount", withdrawTotal);
+    const totalBlance = getOutputValueByID("total_balance");
+    const totalBlanceUpdate = totalBlance - withdrawUserInput;
+    setTextElementById("total_balance", totalBlanceUpdate);
+  } else {
+    alert("Provide correct number");
+  }
 });
